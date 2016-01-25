ac0589083b848bae25681c09e2987bf3fcb55a87
Kristen.Mallory <kristen.mallory@CrossMatch.com>
2016-01-25 10:46:48 -0500

    Order Parameters

--- Dapper.Accelr8.Repo/ver.cs
+++ Dapper.Accelr8.Repo/ver.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.14")]
-[assembly: AssemblyFileVersion("1.0.14")]
+[assembly: AssemblyVersion("1.0.18")]
+[assembly: AssemblyFileVersion("1.0.18")]
